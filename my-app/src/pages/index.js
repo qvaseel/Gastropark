98c5290f55c0f6a5bca338bf90d6a0abd6b1b46a
qvaseel <118725399+qvaseel@users.noreply.github.com>
2024-03-29 13:55:31 +0500

    Delete my-app directory

--- my-app/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import Basket from "./basket/Basket";
-import Home from "./home/Home";
-import Restaurant from "./restaurant/Restaurant";
-
-export { Basket, Restaurant, Home };
--- my-app/src/router/router.js
+++
@@ -1,7 +0,0 @@
-const ROUTER = {
-    KORNER: "/korner",
-    BASKET: "/basket",
-    HOME: "/"
-}
-
-export default ROUTER
